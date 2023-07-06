c886a757d9ff0fb9a5bcd4e21017afe10bd5f9a9
TLJEnthusiast <92526921+TLJEnthusiast@users.noreply.github.com>
2023-07-06 15:08:03 +0200

    Update md_to_html.py

--- uni/2-dis10/md_to_html.py
+++ uni/2-dis10/md_to_html.py
@@ -1,4 +1,8 @@
 import markdown 
+import sys
+
+# richtig relativer markdown path 
+# vielleicht mit CSS Verweis und dann css mit dark mode?
 
 # notes_DIS10_30-06
 
@@ -9,6 +13,9 @@ with open("notes_DIS10_30-06.md", "r") as file:
 # Convert Markdown to HTML
 html = markdown.markdown(markdown_text)
 
+print(html)
+sys.exit()
 # Write the HTML output to a file
 with open('index.html', 'w') as file:
     file.write(html)
+
