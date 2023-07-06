8712f9641b7b4538f96c55fe26e3b4f7c37f67b5
TLJEnthusiast <92526921+TLJEnthusiast@users.noreply.github.com>
2023-07-06 16:04:31 +0200

    responsive mtohtml, darkmode

--- uni/2-dis10/md_to_html.py
+++ uni/2-dis10/md_to_html.py
@@ -1,21 +1,24 @@
-import markdown 
-import sys
+import markdown, os, sys
 
 # richtig relativer markdown path 
 # vielleicht mit CSS Verweis und dann css mit dark mode?
 
-# notes_DIS10_30-06
+current_path = os.path.dirname(os.path.abspath(__file__))
+markdown_path = os.path.join(current_path, "notes_DIS10_30-06.md")
+html_path = os.path.join(current_path, "index.html")
+
 
 # Read the Markdown file
-with open("notes_DIS10_30-06.md", "r") as file:
+with open(markdown_path, "r") as file:
     markdown_text = file.read()
 
 # Convert Markdown to HTML
 html = markdown.markdown(markdown_text)
 
-print(html)
-sys.exit()
+# sys.exit()
 # Write the HTML output to a file
-with open('index.html', 'w') as file:
-    file.write(html)
+with open(html_path, 'w') as file:
+    file.write(f"<!DOCTYPE html>\n<html>\n<head>\n<title>DIS10</title>\n<link rel=\"stylesheet\" type=\"text/css\" href=\"styles/styles.css\">\n</head>\n<body>\n{html}\n</body>")
+
+print("done.")
 
